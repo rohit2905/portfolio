7d0a2c81739e8814c80337ba4750e4e10f74f8fc
Rohit Goud <30547548+rohit2905@users.noreply.github.com>
2020-07-05 16:43:57 +0530

    Add files via upload

---
+++ scripts/modal.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+
+  // MODAL
+  var modalText = {
+    booktalks: {
+      title: 'BookTalks',
+      tag: 'Pet Project',
+      detail: 'Search and Post the reviews on your favourite books and also get free user suggestions and reviews of all available books. NOTE: App might not be functional sometimes due to database security reasons, write me up if required. :)',
+      link: 'http://ec2-52-91-134-149.compute-1.amazonaws.com/'
+    },
+    fooddetection: {
+      title: 'Food Detection',
+      tag: 'Pet Project',
+      detail: 'Recognize Food items, Dishes and ingredients of dish.',
+    link: 'http://ec2-34-229-171-219.compute-1.amazonaws.com/'
+    },
+    facedetection: {
+      title: 'Face Count',
+      tag: 'Pet Project',
+      detail: 'Count number of faces in an image using API calls.',
+      link: 'http://ec2-54-87-39-138.compute-1.amazonaws.com/'
+    },
+    patterndetection: {
+      title: 'Pattern and Texture',
+      tag: 'Pet Project',
+      detail: 'Recognize the type of pattern and texture in an image.',
+      link: 'http://ec2-3-83-217-19.compute-1.amazonaws.com/'
+    }
+  };
+
+  $('#gallery .button').on('click', function(){
+    fillModal(this.id);
+    $('.modal-wrap').addClass('visible');
+  });
+
+  $('.close').on('click', function(){
+    $('.modal-wrap, #modal .button').removeClass('visible');
+  });
+
+  $('.mask').on('click', function(){
+    $('.modal-wrap, #modal .button').removeClass('visible');
+  });
+
+  var carousel = $('#carousel'),
+      slideWidth = 750,
+      threshold = slideWidth/3,
+      dragStart, 
+      dragEnd;
+
+  setDimensions();
+
+  $('#next').click(function(){ shiftSlide(-1) })
+  $('#prev').click(function(){ shiftSlide(1) })
+
+  carousel.on('mousedown', function(){
+    if (carousel.hasClass('transition')) return;
+    dragStart = event.pageX;
+    $(this).on('mousemove', function(){
+      dragEnd = event.pageX;
+      $(this).css('transform','translateX('+ dragPos() +'px)');
+    });
+    $(document).on('mouseup', function(){
+      if (dragPos() > threshold) { return shiftSlide(1) }
+      if (dragPos() < -threshold) { return shiftSlide(-1) }
+      shiftSlide(0);
+    });
+  });
+
+  function setDimensions() {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+     slideWidth = $(window).innerWidth();
+    }
+    $('.carousel-wrap, .slide').css('width', slideWidth);
+    $('.modal').css('max-width', slideWidth);
+    $('#carousel').css('left', slideWidth * -1)
+  }
+
+  function dragPos() {
+    return dragEnd - dragStart;
+  }
+
+  function shiftSlide(direction) {
+    if (carousel.hasClass('transition')) return;
+    dragEnd = dragStart;
+    $(document).off('mouseup')
+    carousel.off('mousemove')
+            .addClass('transition')
+            .css('transform','translateX(' + (direction * slideWidth) + 'px)'); 
+    setTimeout(function(){
+      if (direction === 1) {
+        $('.slide:first').before($('.slide:last'));
+      } else if (direction === -1) {
+        $('.slide:last').after($('.slide:first'));
+      }
+      carousel.removeClass('transition')
+      carousel.css('transform','translateX(0px)'); 
+    },700)
+  }
+
+  function fillModal(id) {
+    $('#modal .title').text(modalText[id].title);
+    $('#modal .detail').text(modalText[id].detail);
+    $('#modal .tag').text(modalText[id].tag);
+    if (modalText[id].link) $('#modal .button').addClass('visible')
+                                               .parent()
+                                               .attr('href', modalText[id].link)
+
+    $.each($('#modal li'), function(index, value ) {
+      $(this).text(modalText[id].bullets[index]);
+    });
+    $.each($('#modal .slide'), function(index, value) {
+      $(this).css({
+        background: "url('img/slides/" + id + '-' + index + ".jpg') center center/cover",
+        backgroundSize: 'cover'
+      });
+              
+    });
+  }
+})
